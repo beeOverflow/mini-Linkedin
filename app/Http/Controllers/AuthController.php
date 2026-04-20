f6d710bf9e6c4662970706b65a4d2effbfa62583
Ahmed <lamghariahmed2026@gmail.com>
2026-04-20 11:11:54 +0100

    admin role fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'role' => 'required|string|in:candidat,recruteur,admin'
+            'role' => 'required|string|in:candidat,recruteur'
         ]);
 
         $user = User::create([
